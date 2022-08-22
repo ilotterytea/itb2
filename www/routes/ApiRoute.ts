d704fa77f36ebb7523945f5114f3ffccde2822df
ilotterytea <ilotterytea@proton.me>
2022-08-23 02:14:24 +0600

    testing

--- www/routes/ApiRoute.ts
+++ www/routes/ApiRoute.ts
@@ -23,6 +23,7 @@ export default function ApiRoute(cfg: IConfiguration): Router {
     const router = Router();
 
     router.post("/gh/webhook", (req, res) => {
+        return res.send(req) // TEST!
         const h_key: string | string[] | undefined = req.headers["x-hub-signature-256"];
         const a_key: string = cfg.Keys.GHWebhook;
         const body: {[key: string]: any} = req.body;
